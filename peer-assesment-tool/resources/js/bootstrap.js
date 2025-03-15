1c5b42581c303f1b758acba91fb0c25681bce539
Mohammed Abdessalam Dakkar <145931944+MohaDak@users.noreply.github.com>
2025-03-15 14:41:32 +0100

    update

--- peer-assesment-tool/resources/js/bootstrap.js
+++ peer-assesment-tool/resources/js/bootstrap.js
@@ -1,7 +1,4 @@
-import './bootstrap';
+import axios from 'axios';
+window.axios = axios;
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
