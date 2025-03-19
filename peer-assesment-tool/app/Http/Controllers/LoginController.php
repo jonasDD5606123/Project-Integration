968fbe58c8cd621481cda923fcb8881ea0da5817
Mohammed Abdessalam Dakkar <145931944+MohaDak@users.noreply.github.com>
2025-03-19 11:50:38 +0100

    update

--- peer-assesment-tool/app/Http/Controllers/LoginController.php
+++ peer-assesment-tool/app/Http/Controllers/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\InlogRequest;
-use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -17,13 +16,8 @@ class LoginController extends Controller
     public function login(InlogRequest $request)
     {
         if (Auth::attempt($request->only('email', 'password'))) {
-            return redirect()->route('dashboard'); // Stuur door naar dashboard
+            return redirect()->route(route: 'dashboard'); // Stuur door naar dashboard
         }
-
-        return back()->withErrors([
-            'email' => 'Ongeldige inloggegevens.',
-            'password' => 'Controleer je wachtwoord en probeer opnieuw.'
-        ]);
     }
 
     public function logout()
